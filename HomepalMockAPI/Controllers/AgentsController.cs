15bc46be7c6c77d2e8fba195a163de6ff82e33cf
djungermann <david.jungermann@gmail.com>
2021-02-16 11:18:34 +0100

    Adds controller for Agent

---
+++ HomepalMockAPI/Controllers/AgentsController.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HomepalMockAPI.DAL;
+using HomepalMockAPI.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace HomepalMockAPI.Controllers
+{
+    [ApiController]
+    [EnableCors]
+    public class AgentsController : ControllerBase
+    {
+        private readonly IAgentsRepository agentsRepository;
+
+        public AgentsController(IAgentsRepository agentsRepository)
+        {
+            this.agentsRepository = agentsRepository;
+        }
+
+        // GET: api/<AgentsController>
+        [Route("api/[controller]")]
+        [HttpGet]
+        public async Task<IEnumerable<Agent>> Get()
+        {
+            return await agentsRepository.Get();
+        }
+
+        // GET: api/<AgentsController>/id
+        [Route("api/[controller]/{id}")]
+        [HttpGet]
+        public async Task<Agent> Get(int id)
+        {
+            return await agentsRepository.Get(id);
+        }
+
+        // POST api/<AgentsController>
+        [Route("api/[controller]")]
+        [HttpPost]
+        public async Task<int> Create([FromBody] Agent agent)
+        {
+            return await agentsRepository.Create(agent);
+        }
+
+        // PUT api/<AgentsController>
+        [Route("api/[controller]")]
+        [HttpPut]
+        public async Task<int> Update([FromBody] Agent agent)
+        {
+            return await agentsRepository.Update(agent);
+        }
+
+        // DELETE: api/<AgentsController>/id
+        [Route("api/[controller]/{id}")]
+        [HttpDelete]
+        public async Task<int> Delete(int id)
+        {
+            return await agentsRepository.Delete(id);
+        }
+    }
+}
