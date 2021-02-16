d76639b463e95a7d066d830e90bec969c97507b6
djungermann <david.jungermann@gmail.com>
2021-02-16 13:04:19 +0100

    Adds content

--- HomepalMockAPI/ClientApp/src/App.tsx
+++ HomepalMockAPI/ClientApp/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import "./App.css";
 function App() {
   return (
-    <div>
-      <p>Här kan vi lägga content!</p>
+    <div className="content">
+      <h1>Om du ser detta funkar API:t</h1>
+      <p>Prova http://localhost:5000/api/leasables/</p>
     </div>
   );
 }
