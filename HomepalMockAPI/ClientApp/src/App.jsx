c86964ab41e3e70c725e3c88adbe96e7e4484292
djungermann <david.jungermann@gmail.com>
2021-02-17 14:21:58 +0100

    Adds Redoc

--- HomepalMockAPI/ClientApp/src/App.jsx
+++ HomepalMockAPI/ClientApp/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css"
 import "./App.css";
+import { RedocStandalone } from 'redoc';
 
 function App() {
   return (
     <div className="content">
-      <SwaggerUI url="http://localhost:5000/swagger/v1/swagger.json"></SwaggerUI>
+      {/* <SwaggerUI url="http://localhost:5000/swagger/v1/swagger.json"></SwaggerUI> */}
+      <RedocStandalone specUrl="http://localhost:5000/swagger/v1/swagger.json"/>
     </div>
   );
 }
