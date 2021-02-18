ea901943944c7250a9dccc5a5708ddb00419d949
djungermann <david.jungermann@gmail.com>
2021-02-18 08:49:57 +0100

    Adds .net build conf

--- HomepalMockAPI/ClientApp/src/App.jsx
+++ HomepalMockAPI/ClientApp/src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css"
+import "swagger-ui-react/swagger-ui.css";
 import "./App.css";
-import { RedocStandalone } from 'redoc';
+import { RedocStandalone } from "redoc";
 
 function App() {
   return (
     <div className="content">
       {/* <SwaggerUI url="http://localhost:5000/swagger/v1/swagger.json"></SwaggerUI> */}
-      <RedocStandalone specUrl="http://localhost:5000/swagger/v1/swagger.json"/>
+      <RedocStandalone
+        specUrl="http://localhost:5000/swagger/v1/swagger.json"
+        options={{
+          theme: { colors: { primary: { main: "#000000" } } },
+        }}
+      />
     </div>
   );
 }
