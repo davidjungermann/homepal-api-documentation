f412d588d0b6f0f408521f074f02d26520145e67
djungermann <david.jungermann@gmail.com>
2021-03-19 10:37:29 +0100

    Removes documentation libraries

--- HomepalMockAPI/ClientApp/src/App.jsx
+++ HomepalMockAPI/ClientApp/src/App.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import SwaggerUI from "swagger-ui-react";
-import "swagger-ui-react/swagger-ui.css";
 import "./App.css";
-import { RedocStandalone } from "redoc";
+
 
 function App() {
   return (
     <div className="content">
-      {/* <SwaggerUI url="http://localhost:5000/swagger/v1/swagger.json"></SwaggerUI> */}
-      <RedocStandalone
-        specUrl="http://localhost:5000/swagger/v1/swagger.json"
-        options={{
-          theme: { colors: { primary: { main: "#000000" } } },
-        }}
-      />
+      <h1>Tja</h1>
     </div>
   );
 }
