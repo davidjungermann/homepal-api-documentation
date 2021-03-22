77f8e544e3c37afd0bebb818d82f5c0a7a6e4a0e
djungermann <david.jungermann@gmail.com>
2021-03-22 10:45:03 +0100

    Select item from navbar

--- HomepalMockAPI/ClientApp/src/components/navigation/Navigation.js
+++ HomepalMockAPI/ClientApp/src/components/navigation/Navigation.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navigation.scss";
 import List from "@material-ui/core/List";
 import { NavigationItem } from "./item/NavigationItem";
 
-export const Navigation = ({ items, depthStep, depth }) => {
+export const Navigation = ({ items }) => {
+  const [currentNavigationItem, setCurrentNavigationItem] = useState("Init");
+
+  const handleCurrentNavigationItem = (value) => {
+    setCurrentNavigationItem(value);
+  };
+
   return (
     <React.Fragment>
+      <h1>{currentNavigationItem}</h1>
       <div className="navigation-container">
         <List disablePadding dense>
           {items.map((sidebarItem, index) => (
             <NavigationItem
+              handleCurrentNavigationItem={handleCurrentNavigationItem}
               key={`${sidebarItem.label}${index}`}
-              depthStep={depthStep}
-              depth={depth}
               {...sidebarItem}
             />
           ))}
--- HomepalMockAPI/ClientApp/src/components/navigation/item/NavigationItem.js
+++ HomepalMockAPI/ClientApp/src/components/navigation/item/NavigationItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import "./NavigationItem.scss";
 
 export const NavigationItem = ({
   label,
@@ -9,23 +10,32 @@ export const NavigationItem = ({
   depthStep = 15,
   depth = 0,
   fontSize = 18,
+  handleCurrentNavigationItem,
   ...rest
 }) => {
   return (
     <React.Fragment>
-      <ListItem button dense {...rest}>
+      <ListItem
+        button
+        onClick={(e) => handleCurrentNavigationItem(label)}
+        {...rest}
+      >
         <ListItemText style={{ paddingLeft: depth * depthStep }}>
-          <span style={{ fontSize: fontSize }}>{label}</span>
+          <span className="list-item-label" style={{ fontSize: fontSize }}>
+            {label}
+          </span>
         </ListItemText>
       </ListItem>
       {Array.isArray(items) ? (
-        <List disablePadding dense>
+        <List disablePadding>
           {items.map((subItem) => (
             <NavigationItem
+              onClick={() => handleCurrentNavigationItem(subItem.label)}
               key={subItem.label}
               depth={depth + 1}
               depthStep={depthStep}
               fontSize={fontSize - 2}
+              handleCurrentNavigationItem={handleCurrentNavigationItem}
               {...subItem}
             />
           ))}
