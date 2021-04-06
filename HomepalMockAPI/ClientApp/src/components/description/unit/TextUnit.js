abd71e006d7af0d006936f6cceb5587e011305f0
djungermann <david.jungermann@gmail.com>
2021-04-06 15:03:13 +0200

    Updates hash

--- HomepalMockAPI/ClientApp/src/components/description/unit/TextUnit.js
+++ HomepalMockAPI/ClientApp/src/components/description/unit/TextUnit.js
@@ -1,21 +1,21 @@
 import React from "react";
 import handleViewport from "react-in-viewport";
+import { useHistory } from "react-router-dom";
 
 import "./TextUnit.scss";
 
 const TextUnitContent = (props) => {
-  const { forwardedRef, inViewport, enterCount } = props;
+  const { forwardedRef, inViewport } = props;
+  let history = useHistory();
 
   if (inViewport) {
-    console.log(forwardedRef);
+    console.log(props.id);
+    history.push("/#" + props.id);
+    console.log(history);
   }
 
   return (
-    <div
-      id={props.id}
-      className="unit-container"
-      ref={forwardedRef}
-    >
+    <div id={props.id} className="unit-container" ref={forwardedRef}>
       <h1>{props.header}</h1>
       <p>{props.content}</p>
     </div>
