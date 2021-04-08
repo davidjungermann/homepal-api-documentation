4e6e60d0e072f3a4ab29d7e154ade9846d8aac8f
Axel Holmqvist <axelholmqvist@Axels-MacBook-Pro.local>
2021-04-08 14:50:18 +0200

    2 column styling

--- HomepalMockAPI/ClientApp/src/components/content/Routes.js
+++ HomepalMockAPI/ClientApp/src/components/content/Routes.js
@@ -26,20 +26,34 @@ export const routes = [
   {
     main: (history) => (
       <Section id="about-the-api" history={history}>
+        <Description>
         <TextUnit
           header="About the API"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
         ></TextUnit>
+        </Description>
+        <Usage>
+          <ContainerBlock header="Example">
+            curl http://localhost:5000/api/agents
+          </ContainerBlock>
+        </Usage>
       </Section>
     ),
   },
   {
     main: (history) => (
       <Section id="conceptual-information" history={history}>
+        <Description>
         <TextUnit
           header="Conceptual information"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
         ></TextUnit>
+        </Description>
+        <Usage>
+          <ContainerBlock header="Example">
+            curl http://localhost:5000/api/agents
+          </ContainerBlock>
+        </Usage>
       </Section>
     ),
   },
