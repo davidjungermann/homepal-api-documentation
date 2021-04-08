9ff70266c47ee92213059e2d66361e381dadd770
djungermann <david.jungermann@gmail.com>
2021-04-08 13:03:16 +0200

    Refact

--- HomepalMockAPI/ClientApp/src/components/description/Routes.js
+++ HomepalMockAPI/ClientApp/src/components/description/Routes.js
@@ -5,132 +5,120 @@ import { Section } from "../section/Section";
 export const routes = [
   {
     main: (history) => (
-      <Section id="introduction">
+      <Section id="introduction" history={history}>
         <TextUnit
           header="Introduction"
           content="Lorem ipsum dolor sit amet, consectetur adi. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="about-the-api">
+      <Section id="about-the-api" history={history}>
         <TextUnit
           header="About the API"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="conceptual-information">
+      <Section id="conceptual-information" history={history}>
         <TextUnit
           header="Conceptual information"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="authorization">
+      <Section id="authorization" history={history}>
         <TextUnit
           header="Authorization"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="validations">
+      <Section id="validations" history={history}>
         <TextUnit
           header="Validations"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="errors">
+      <Section id="errors" history={history}>
         <TextUnit
           header="Errors"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="query-parameters">
+      <Section id="query-parameters" history={history}>
         <TextUnit
           header="Query parameters"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="pagination">
+      <Section id="pagination" history={history}>
         <TextUnit
           header="Pagination"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section>
+      <Section id="sorting" history={history}>
         <TextUnit
           header="Sorting"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="inclusion">
+      <Section id="inclusion" history={history}>
         <TextUnit
           header="Inclusion"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="filter">
+      <Section id="filter" history={history}>
         <TextUnit
           header="Filter"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
   },
   {
     main: (history) => (
-      <Section id="versioning">
+      <Section id="versioning" history={history}>
         <TextUnit
           header="Versioning"
           content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis augue nec augue vestibulum, eu sodales nisl maximus. Nam ut fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut, gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula augue a, ullamcorper nulla. Praesent lacinia risus quis urna tristique, eu gravida leo consectetur. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit amet congue ipsum viverra. Ut semper vitae felis nec porta. Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor"
-          history={history}
         ></TextUnit>
       </Section>
     ),
--- HomepalMockAPI/ClientApp/src/components/section/Section.js
+++ HomepalMockAPI/ClientApp/src/components/section/Section.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import handleViewport from "react-in-viewport";
 
 import "./Section.scss";
 
-export const Section = ({ children }) => {
-  return <div className="section-container">{children}</div>;
+export const SectionContent = ({ props }) => {
+  const { forwardedRef, inViewport } = props;
+  var [height, setHeight] = useState(0);
+
+  if (inViewport) {
+    props.history.history.push("/#" + props.id);
+  }
+
+  return (
+    <div className="section-container" ref={forwardedRef}>
+      {props.children}
+    </div>
+  );
 };
+
+export const Section = handleViewport(SectionContent, {
+  rootMargin: "100px 0px -20% 0px",
+  threshold: 1,
+});
