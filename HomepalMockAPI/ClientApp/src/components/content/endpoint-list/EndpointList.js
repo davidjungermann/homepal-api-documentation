834a58458081f4aa2e32ec7c87bcab8f6677a7c8
djungermann <david.jungermann@gmail.com>
2021-04-09 10:36:11 +0200

    Maps over endpoints

--- HomepalMockAPI/ClientApp/src/components/content/endpoint-list/EndpointList.js
+++ HomepalMockAPI/ClientApp/src/components/content/endpoint-list/EndpointList.js
@@ -3,5 +3,13 @@ import { ContainerBlock } from "../../container-block/ContainerBlock";
 import "./EndpointList.scss";
 
 export const EndpointList = ({ endpoints }) => {
-  return <ContainerBlock header="Endpoints" copy>{endpoints}</ContainerBlock>;
+  return (
+    <ContainerBlock header="Endpoints" copy>
+      <ul className="navigation-sub-list">
+        {endpoints.map((endpoint) => {
+          return <li>{endpoint}</li>;
+        })}
+      </ul>
+    </ContainerBlock>
+  );
 };
