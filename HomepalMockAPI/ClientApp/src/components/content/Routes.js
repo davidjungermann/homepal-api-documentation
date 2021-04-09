c0c1edb3a934edffe4b44f5848c6a2dabc7d7aa7
djungermann <david.jungermann@gmail.com>
2021-04-09 15:43:29 +0200

    Body object finished

--- HomepalMockAPI/ClientApp/src/components/content/Routes.js
+++ HomepalMockAPI/ClientApp/src/components/content/Routes.js
@@ -8,7 +8,11 @@ import { EndpointList } from "../content/endpoint-list/EndpointList";
 import { Endpoint } from "../content/endpoint/Endpoint";
 import { BodyDescription } from "../content/body-description/BodyDescription";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCopy, faExclamationTriangle, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCopy,
+  faExclamationTriangle,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const routes = [
   {
@@ -17,26 +21,45 @@ export const routes = [
         <Description>
           <TextUnit header="About the API">
             <p>
-              Homebase API connects you to all the data you need within the property and real estate business to build innovative applications – or simply carry out extensive analytics.
+              Homebase API connects you to all the data you need within the
+              property and real estate business to build innovative applications
+              – or simply carry out extensive analytics.
             </p>
             <p>
-              This document describes the Homebase API, which is an API based on <a href="https://restfulapi.net/">REST principles</a>. 
-              All routes described are protected and requires a token-based authentication performed via <a href="https://oauth.net/">OAuth</a>. 
-              The responses of the API are based on <a href="https://jsonapi.org">https://jsonapi.org</a>. 
-              Error handling is done via <a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes">standard HTTP status codes</a>. 
-              Lastly, the architectural structure is based on Real Estate Core, which is an open ontology for real estate standards.
+              This document describes the Homebase API, which is an API based on{" "}
+              <a href="https://restfulapi.net/">REST principles</a>. All routes
+              described are protected and requires a token-based authentication
+              performed via <a href="https://oauth.net/">OAuth</a>. The
+              responses of the API are based on{" "}
+              <a href="https://jsonapi.org">https://jsonapi.org</a>. Error
+              handling is done via{" "}
+              <a href="https://en.wikipedia.org/wiki/List_of_HTTP_status_codes">
+                standard HTTP status codes
+              </a>
+              . Lastly, the architectural structure is based on Real Estate
+              Core, which is an open ontology for real estate standards.
             </p>
             <p>
-              You can use the Homebase API in <a href="#authorization">test mode</a>, which does not affect your live data or interact with the banking networks.
+              You can use the Homebase API in{" "}
+              <a href="#authorization">test mode</a>, which does not affect your
+              live data or interact with the banking networks.
             </p>
             <p>
-              The documentation is organized by resource type. For each resource type, the documentation lists the available endpoints, one or more data representations, JSON objects, the supported methods (GET, POST, PATCH, DELETE), as well as at least one usage example.
+              The documentation is organized by resource type. For each resource
+              type, the documentation lists the available endpoints, one or more
+              data representations, JSON objects, the supported methods (GET,
+              POST, PATCH, DELETE), as well as at least one usage example.
             </p>
             <p className="small">
-              <FontAwesomeIcon icon={faInfoCircle} /> In order to use our live API you need be a customer of Homepal, and also have connected one or more data sources to the system.
+              <FontAwesomeIcon icon={faInfoCircle} /> In order to use our live
+              API you need be a customer of Homepal, and also have connected one
+              or more data sources to the system.
             </p>
             <p className="small">
-              <FontAwesomeIcon icon={faExclamationTriangle} /> The API changes every now and then, and endpoints are depricated. If you are using the API make sure to check in on Deprication from time to time. We will of course notice you in advance!
+              <FontAwesomeIcon icon={faExclamationTriangle} /> The API changes
+              every now and then, and endpoints are depricated. If you are using
+              the API make sure to check in on Deprication from time to time. We
+              will of course notice you in advance!
             </p>
           </TextUnit>
         </Description>
@@ -54,6 +77,7 @@ export const routes = [
             ></Endpoint>
           </EndpointList>
           <BodyDescription
+            header={"Array object example"}
             copy
             body={[
               {
@@ -83,6 +107,7 @@ export const routes = [
             ]}
           ></BodyDescription>
           <BodyDescription
+            header={"Single object example"}
             copy
             body={{
               id: 1,
