2aa1a748fcb9e30330837c693959fde652d5e717
axelholmqvist <axelholmqvist8@gmail.com>
2021-04-13 16:39:33 +0200

    Ok Architectural Overview shell.

--- HomepalMockAPI/ClientApp/src/components/content/architectural-overview/ArchitecturalOverview.js
+++ HomepalMockAPI/ClientApp/src/components/content/architectural-overview/ArchitecturalOverview.js
@@ -1,13 +1,126 @@
 import React from "react";
 import "./ArchitecturalOverview.scss";
 
+import { ArcherContainer, ArcherElement } from 'react-archer';
+
 export const ArchitecturalOverview = ({ history }) => {
 
-  const link = "";
+  const link = "get-buildings";
 
   return (
     <div className="architectural-overview-container">
-      <div onClick={() => history.history.push("#" + link)}>Test</div>
+      <ArcherContainer strokeColor='#e0e8f0' strokeWidth={1}>
+      <div className="arch-container">
+        <div className="arch-column">
+          <div className="arch-row">
+            <ArcherElement id="region">
+              <div className="architectural-overview-badge">Region</div>
+            </ArcherElement>
+          </div>
+          <div className="arch-row">
+            <ArcherElement 
+              id="real-estate"
+              relations={[{
+                targetId: 'region',
+                targetAnchor: 'bottom',
+                sourceAnchor: 'top',
+                style: { endShape: {"circle":{"radius":0,"fillColor":"#b5e48c","strokeColor":"#e0e8f0","strokeWidth":0}} },
+              },{
+                targetId: 'owner',
+                targetAnchor: 'left',
+                sourceAnchor: 'right',
+                style: { endShape: {"circle":{"radius":0,"fillColor":"#b5e48c","strokeColor":"#e0e8f0","strokeWidth":0}} },
+              }]}>
+              <div className="architectural-overview-badge">Real Estate</div>
+            </ArcherElement>
+          </div>
+          <div className="arch-row">
+            <ArcherElement
+              id="building"
+              relations={[{
+                targetId: 'real-estate',
+                targetAnchor: 'bottom',
+                sourceAnchor: 'top',
+                style: { endShape: {"circle":{"radius":0,"fillColor":"#b5e48c","strokeColor":"#e0e8f0","strokeWidth":0}} },
+              }]}
+            >
+              <div className="architectural-overview-badge">Building</div>
+            </ArcherElement>
+          </div>
+        </div>
+        <div className="arch-column">
+          <div className="arch-row">
+          
+          </div>
+          <div className="arch-row">
+            <ArcherElement 
+              id="owner"
+              relations={[{
+                targetId: 'agent',
+                targetAnchor: 'bottom',
+                sourceAnchor: 'top',
+              }]}>
+              <div className="architectural-overview-badge">Owner</div>
+            </ArcherElement>
+          </div>
+          <div className="arch-row">
+          
+          </div>
+        </div>
+        <div className="arch-column">
+          <div className="arch-row">
+            <ArcherElement id="agent">
+              <div className="architectural-overview-badge">Agent</div>
+            </ArcherElement>
+          </div>
+          <div className="arch-row">
+          
+          </div>
+          <div className="arch-row">
+            <ArcherElement
+              id="leasable"
+              relations={[{
+                targetId: 'building',
+                targetAnchor: 'right',
+                sourceAnchor: 'left',
+                style: { endShape: {"circle":{"radius":0,"fillColor":"#b5e48c","strokeColor":"#e0e8f0","strokeWidth":0}} },
+              },{
+                targetId: 'owner',
+                targetAnchor: 'bottom',
+                sourceAnchor: 'top',
+                style: { endShape: {"circle":{"radius":0,"fillColor":"#b5e48c","strokeColor":"#e0e8f0","strokeWidth":0}} },
+              },{
+                targetId: 'customer',
+                targetAnchor: 'bottom',
+                sourceAnchor: 'top',
+                style: { endShape: {"circle":{"radius":0,"fillColor":"#b5e48c","strokeColor":"#e0e8f0","strokeWidth":0}} },
+              }]}
+            >
+              <div className="architectural-overview-badge">Leasable</div>
+            </ArcherElement>
+          </div>
+        </div>
+        <div className="arch-column">
+          <div className="arch-row">
+              
+          </div>
+          <div className="arch-row">
+            <ArcherElement 
+              id="customer"
+              relations={[{
+                targetId: 'agent',
+                targetAnchor: 'bottom',
+                sourceAnchor: 'top',
+              }]}>
+              <div className="architectural-overview-badge">Customer</div>
+            </ArcherElement>
+          </div>
+          <div className="arch-row">
+          
+          </div>
+        </div>
+      </div>
+      </ArcherContainer>
     </div>
   );
 };
