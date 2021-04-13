b4852923955c1afa3abd61646b96ff2da221bd45
djungermann <david.jungermann@gmail.com>
2021-04-13 10:29:09 +0200

    Adds auth

--- HomepalMockAPI/ClientApp/src/components/authorization/Authorization.js
+++ HomepalMockAPI/ClientApp/src/components/authorization/Authorization.js
@@ -4,15 +4,30 @@ import "./Authorization.scss";
 
 export const Authorization = () => {
   const [token, setToken] = useState("");
+  const [isValid, setIsValid] = useState("");
 
   const fetchToken = async () => {
     const response = await axios.get("https://localhost:6001/api/authtoken");
     setToken(response.data.value);
   };
+
+  const validateToken = async () => {
+    const response = await axios.get(
+      "https://localhost:6001/api/authtoken/" + token
+    );
+    setIsValid(response.data.isValid.toString());
+    console.log(response.data.isValid);
+  };
   return (
     <div className="authorization-container">
       <p className="token">{token}</p>
-      <button onClick={fetchToken}>Generate authorization token</button>
+      {!token && (
+        <button onClick={fetchToken}>Generate authorization token</button>
+      )}
+      {token && (
+        <button onClick={validateToken}>Validate authorization token</button>
+      )}
+      <p className="is-valid">Validity: {isValid}</p>
     </div>
   );
 };
