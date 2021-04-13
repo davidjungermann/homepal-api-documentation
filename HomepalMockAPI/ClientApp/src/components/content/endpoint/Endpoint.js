732a37da72d4f26657a9614895e73bccb2b683e7
djungermann <david.jungermann@gmail.com>
2021-04-13 14:14:32 +0200

    Prop structure

--- HomepalMockAPI/ClientApp/src/components/content/endpoint/Endpoint.js
+++ HomepalMockAPI/ClientApp/src/components/content/endpoint/Endpoint.js
@@ -4,6 +4,7 @@ import "./Endpoint.scss";
 export const Endpoint = ({ request, baseUrl, parameter, link, history }) => {
   const requestName = request.toLowerCase() + "-request";
 
+  console.log(history);
   if (parameter === undefined) {
     parameter = "";
   }
@@ -15,7 +16,7 @@ export const Endpoint = ({ request, baseUrl, parameter, link, history }) => {
   return (
     <div
       className="endpoint-container"
-      onClick={() => history.history.push("#" + link)}
+      onClick={() => history.push("#" + link)}
     >
       <span className={requestName + " request"}>{request}</span>
       <span className="request-url">{baseUrl + parameter}</span>
