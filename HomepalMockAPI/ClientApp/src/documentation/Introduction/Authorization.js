d96161e1112e8f0c5e8d0501f0ef830b3f434472
axelholmqvist <axelholmqvist8@gmail.com>
2021-04-15 10:46:59 +0200

    Push for David.

--- HomepalMockAPI/ClientApp/src/documentation/Introduction/Authorization.js
+++ HomepalMockAPI/ClientApp/src/documentation/Introduction/Authorization.js
@@ -5,6 +5,7 @@ import { Description } from "../../components/description/Description";
 import { Section } from "../../components/section/Section";
 import { Usage } from "../../components/usage/Usage";
 import { Auth } from "../../components/auth/Auth";
+import { SectionWrapper } from "../../components/content/section-wrapper/SectionWrapper";
 
 export const Authorization = ({
   history,
@@ -14,19 +15,34 @@ export const Authorization = ({
   setValid,
 }) => {
   return (
-    <Section id="authorization" history={history}>
-      <Description>
-        <TextUnit header="Authorization"></TextUnit>
-      </Description>
-      <Usage>
-        {" "}
-        <Auth
-          token={token}
-          setToken={setToken}
-          valid={valid}
-          setValid={setValid}
-        ></Auth>
-      </Usage>
-    </Section>
+    <SectionWrapper>
+      <Section id="authorization" history={history}>
+        <Description>
+          <TextUnit header="Authorization">
+          <p>
+          All routes described in the reference documentation are protected and can only be reached by a trusted client and with an access token. 
+          </p>
+          <p>
+          The Homebase API uses token-based authentication that is performed via OAuth. API tokens must be included in the header of all requests made to the API. All API requests must be made over HTTPS. Calls made over plain HTTP will fail. API requests without authentication will also fail.
+          </p>
+          <p>
+          You can generate tokens and find your existing ones in your <a>dashboard</a>, or read more about it below in <a>Live mode access tokens</a>.
+          </p>
+          <p>
+          You can make requests within the documentation, in the sandbox, or on your own, by using test tokens to test our API before you go live. Read more about it below in <a>Test mode access tokens</a>.
+          </p>
+          </TextUnit>
+        </Description>
+        <Usage>
+          {" "}
+          <Auth
+            token={token}
+            setToken={setToken}
+            valid={valid}
+            setValid={setValid}
+          ></Auth>
+        </Usage>
+      </Section>
+    </SectionWrapper>
   );
 };
