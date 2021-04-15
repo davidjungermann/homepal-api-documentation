b61ab0a29244fd7747451ac15d853c7bc1cbd68b
djungermann <david.jungermann@gmail.com>
2021-04-15 09:10:06 +0200

    Fixes scolling

--- HomepalMockAPI/ClientApp/src/components/content/architectural-overview/ArchitecturalObject.js
+++ HomepalMockAPI/ClientApp/src/components/content/architectural-overview/ArchitecturalObject.js
@@ -10,30 +10,49 @@ export const ArchitecturalObject = ({
   cardinalities,
   additionalClass,
 }) => {
-  if (link === undefined) {
-    link = "#";
-  }
-
   if (additionalClass === undefined) {
     additionalClass = "";
   }
 
-  return (
-    <div className="architectural-object">
-      {cardinalities &&
-        cardinalities.map((cardinality) => {
-          return (
-            <div className={"cardinality cardinality-" + cardinality[0]}>
-              {cardinality[1]}
-            </div>
-          );
-        })}
-      <Link activeClassName="not-active" smooth to={"#" + link}>
+  const renderObject = () => {
+    return (
+      <div className="architectural-object">
+        {cardinalities &&
+          cardinalities.map((cardinality) => {
+            return (
+              <div className={"cardinality cardinality-" + cardinality[0]}>
+                {cardinality[1]}
+              </div>
+            );
+          })}
         <div className={"architectural-overview-badge " + additionalClass}>
           <div>{children}</div>
           <div>{title}</div>
         </div>
-      </Link>
-    </div>
-  );
+      </div>
+    );
+  };
+
+  const renderLinkObject = () => {
+    return (
+      <div className="architectural-object">
+        {cardinalities &&
+          cardinalities.map((cardinality) => {
+            return (
+              <div className={"cardinality cardinality-" + cardinality[0]}>
+                {cardinality[1]}
+              </div>
+            );
+          })}
+        <Link activeClassName="not-active" smooth to={"#" + link}>
+          <div className={"architectural-overview-badge " + additionalClass}>
+            <div>{children}</div>
+            <div>{title}</div>
+          </div>
+        </Link>
+      </div>
+    );
+  };
+
+  return link ? renderLinkObject() : renderObject();
 };
