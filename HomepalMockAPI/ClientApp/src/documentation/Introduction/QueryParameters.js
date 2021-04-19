85b9816a618bd3834861929b05e9be98e31e517c
axelholmqvist <axelholmqvist8@gmail.com>
2021-04-19 10:31:43 +0200

    Fix.

--- HomepalMockAPI/ClientApp/src/documentation/Introduction/QueryParameters.js
+++ HomepalMockAPI/ClientApp/src/documentation/Introduction/QueryParameters.js
@@ -14,24 +14,7 @@ export const QueryParameters = ({ history }) => {
         <Description>
           <TextUnit header="Query Parameters">
             <p>
-<<<<<<< HEAD
               Homebase utilizes query parameters for presenting the returned data in a specific way.
-=======
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis
-              augue nec augue vestibulum, eu sodales nisl maximus. Nam ut
-              fermentum augue. Vivamus diam tortor, ultrices sit amet bibendum ut,
-              gravida vel tortor. Vivamus a mollis est. Maecenas varius tempus
-              nisi, at mattis sapien rutrum ut. Cras sed sem egestas, vehicula
-              augue a, ullamcorper nulla. Praesent lacinia risus quis urna
-              tristique, eu gravida leo consectetur. Orci varius natoque penatibus
-              et magnis dis parturient montes, nascetur ridiculus mus. Orci varius
-              natoque penatibus et magnis dis parturient montes, nascetur
-              ridiculus mus. Cras imperdiet ultricies metus. Sed vel velit eget
-              arcu hendrerit sollicitudin congue quis ex. Curabitur ullamcorper
-              vulputate facilisis. Aliquam porttitor nunc nec urna accumsan, sit
-              amet congue ipsum viverra. Ut semper vitae felis nec porta.
-              Suspendisse potenti. Vestibulum mattis tristique dolor. Ipsum Dolor
->>>>>>> 8d9a582dd4511031c87a1a3ea0dc310562577d16
             </p>
           </TextUnit>
         </Description>
