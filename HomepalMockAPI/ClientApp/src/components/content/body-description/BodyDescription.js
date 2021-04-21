f5e93db4380e7bbd03e28c1a56701cba90736d1b
djungermann <david.jungermann@gmail.com>
2021-04-21 08:17:23 +0200

    Simplifies render of objects

--- HomepalMockAPI/ClientApp/src/components/content/body-description/BodyDescription.js
+++ HomepalMockAPI/ClientApp/src/components/content/body-description/BodyDescription.js
@@ -5,117 +5,16 @@ import nextId from "react-id-generator";
 import "./BodyDescription.scss";
 
 export const BodyDescription = ({ header, body, copy }) => {
-  const [isArray] = useState(Array.isArray(body));
-  const [content] = useState(body);
+  const [content] = useState(JSON.stringify(body, null, " "));
   const [copied, setCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(JSON.stringify(content, null, " "));
+    navigator.clipboard.writeText(content);
     setCopied(true);
   };
 
   const handleLeave = () => {
     setCopied(false);
-  };
-
-  const renderBody = () => {
-    return Object.entries(body).map(([key, value], index) => {
-      if (index === 0 && Object.keys(body).length !== 1) {
-        return (
-          <div key={nextId()} className="first-row">
-            {openingBrace()}"{key}": {value}
-          </div>
-        );
-      } else if (
-        index === Object.keys(body).length - 1 &&
-        Object.keys(body).length !== 1
-      ) {
-        return (
-          <div key={nextId()} className="last-row">
-            "{key}": {value}
-            {closingBrace()}
-          </div>
-        );
-      } else if (index === 0 && Object.keys(body).length === 1) {
-        return (
-          <div key={nextId()} className="first-and-last-row">
-            {openingBrace()}"{key}": {value}
-            {closingBrace()}
-          </div>
-        );
-      } else {
-        return (
-          <div key={nextId()} className="middle-row">
-            "{key}": {value}
-          </div>
-        );
-      }
-    });
-  };
-
-  const renderArrayBody = () => {
-    return body.map((obj, arrayIndex) => {
-      return Object.entries(obj).map(([key, value], objIndex) => {
-        if (objIndex === 0 && arrayIndex === 0) {
-          return (
-            <div key={nextId()} className="first-object-row">
-              {openingBracket()}
-              {openingBrace()}"{key}": {value}
-            </div>
-          );
-        } else if (
-          objIndex === Object.keys(obj).length - 1 &&
-          arrayIndex === body.length - 1
-        ) {
-          return (
-            <div key={nextId()} className="last-object-row">
-              "{key}": {value}
-              {closingBrace()}
-              {closingBracket()}
-            </div>
-          );
-        } else if (objIndex === 0) {
-          return (
-            <div key={nextId()} className="first-row">
-              {openingBrace()}"{key}": {value}
-            </div>
-          );
-        } else if (objIndex === Object.keys(obj).length - 1) {
-          return (
-            <div key={nextId()} className="last-row">
-              "{key}": {value}
-              {continuingBrace()}
-            </div>
-          );
-        } else {
-          return (
-            <div key={nextId()} className="middle-row">
-              "{key}": {value}
-            </div>
-          );
-        }
-      });
-    });
-  };
-
-  const openingBrace = () => {
-    return <div className="opening-brace">{"{"}</div>;
-  };
-
-  const closingBrace = () => {
-    return <div className="closing-brace">{"}"}</div>;
-  };
-
-  const continuingBrace = () => {
-    return <div className="closing-brace">{"},"}</div>;
-  };
-
-  const openingBracket = () => {
-    return <div className="opening-bracket">{"["}</div>;
-  };
-
-  const closingBracket = () => {
-    return <div className="closing-bracket">{"]"}</div>;
   };
 
   return (
@@ -126,7 +25,7 @@ export const BodyDescription = ({ header, body, copy }) => {
       copied={copied}
       handleLeave={handleLeave}
     >
-      {isArray ? renderArrayBody : renderBody}
+      {content}
     </ContainerBlock>
   );
 };
