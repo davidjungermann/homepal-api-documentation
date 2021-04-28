07d44263d3f49a7fdd4251f4f6c034a4c3a8d6b4
davidjungermann <david.jungermann@gmail.com>
2021-04-28 10:55:01 +0200

    Comments away

--- HomepalMockAPI/ClientApp/src/components/navigation/item/NavigationItem.js
+++ HomepalMockAPI/ClientApp/src/components/navigation/item/NavigationItem.js
@@ -16,123 +16,123 @@ export const NavigationItem = ({
 }) => {
   const [open, setOpen] = useState(
     depth === 1 || depth === 2
-      ? "navigation-sub-list-closed"
+      ? "navigation-sub-list-open"
       : "navigation-sub-list-open"
   );
 
-  history.listen((location) => {
-    if (depth === 1 || depth === 2) {
-      if (
-        open === "navigation-sub-list-open" &&
-        "#" + formatUrl(label) !== location.hash
-      ) {
-        // This is a terrible solution. TODO: Fix!
-        if (
-          location.hash === "#conceptual-overview" ||
-          location.hash === "#database-properties" ||
-          location.hash === "#enumerated-values" ||
-          location.hash === "#generate-access-key" ||
-          location.hash === "#error-codes" ||
-          location.hash === "#pagination" ||
-          location.hash === "#sorting" ||
-          location.hash === "#upcoming-deprecations" ||
-          location.hash === "#get-agents" ||
-          location.hash === "#get-agent" ||
-          location.hash === "#create-agent" ||
-          location.hash === "#update-agent" ||
-          location.hash === "#delete-agent" ||
-          location.hash === "#get-buildings" ||
-          location.hash === "#get-building" ||
-          location.hash === "#create-building" ||
-          location.hash === "#update-building" ||
-          location.hash === "#delete-building" ||
-          location.hash === "#get-customers" ||
-          location.hash === "#get-customer" ||
-          location.hash === "#create-customer" ||
-          location.hash === "#update-customer" ||
-          location.hash === "#delete-customer" ||
-          location.hash === "#get-leasables" ||
-          location.hash === "#get-leasable" ||
-          location.hash === "#create-leasable" ||
-          location.hash === "#update-leasable" ||
-          location.hash === "#delete-leasable" ||
-          location.hash === "#get-owners" ||
-          location.hash === "#get-owner" ||
-          location.hash === "#create-owner" ||
-          location.hash === "#update-owner" ||
-          location.hash === "#delete-owner" ||
-          location.hash === "#get-realestates" ||
-          location.hash === "#get-realestate" ||
-          location.hash === "#create-realestate" ||
-          location.hash === "#update-realestate" ||
-          location.hash === "#delete-realestate" ||
-          location.hash === "#get-regions" ||
-          location.hash === "#create-region" ||
-          location.hash === "#delete-region"
-        ) {
-          setOpen("navigation-sub-list-open");
-          console.log("haj");
-        } else {
-          setOpen("navigation-sub-list-closed");
-        }
-      } else if (
-        open === "navigation-sub-list-closed" &&
-        "#" + formatUrl(label) === location.hash
-      ) {
-        if (
-          location.hash === "#conceptual-overview" ||
-          location.hash === "#database-properties" ||
-          location.hash === "#enumerated-values" ||
-          location.hash === "#generate-access-key" ||
-          location.hash === "#error-codes" ||
-          location.hash === "#pagination" ||
-          location.hash === "#sorting" ||
-          location.hash === "#upcoming-deprecations" ||
-          location.hash === "#get-agents" ||
-          location.hash === "#get-agent" ||
-          location.hash === "#create-agent" ||
-          location.hash === "#update-agent" ||
-          location.hash === "#delete-agent" ||
-          location.hash === "#get-buildings" ||
-          location.hash === "#get-building" ||
-          location.hash === "#create-building" ||
-          location.hash === "#update-building" ||
-          location.hash === "#delete-building" ||
-          location.hash === "#get-customers" ||
-          location.hash === "#get-customer" ||
-          location.hash === "#create-customer" ||
-          location.hash === "#update-customer" ||
-          location.hash === "#delete-customer" ||
-          location.hash === "#get-leasables" ||
-          location.hash === "#get-leasable" ||
-          location.hash === "#create-leasable" ||
-          location.hash === "#update-leasable" ||
-          location.hash === "#delete-leasable" ||
-          location.hash === "#get-owners" ||
-          location.hash === "#get-owner" ||
-          location.hash === "#create-owner" ||
-          location.hash === "#update-owner" ||
-          location.hash === "#delete-owner" ||
-          location.hash === "#get-realestates" ||
-          location.hash === "#get-realestate" ||
-          location.hash === "#create-realestate" ||
-          location.hash === "#update-realestate" ||
-          location.hash === "#delete-realestate" ||
-          location.hash === "#get-regions" ||
-          location.hash === "#create-region" ||
-          location.hash === "#delete-region"
-        ) {
-          setOpen("navigation-sub-list-closed");
-        }
-        setOpen("navigation-sub-list-open");
-      } else if (
-        open === "navigation-sub-list-closed" &&
-        "#" + formatUrl(label) !== location.hash
-      ) {
-        setOpen("navigation-sub-list-closed");
-      }
-    }
-  });
+  // history.listen((location) => {
+  //   if (depth === 1 || depth === 2) {
+  //     if (
+  //       open === "navigation-sub-list-open" &&
+  //       "#" + formatUrl(label) !== location.hash
+  //     ) {
+  //       // This is a terrible solution. TODO: Fix!
+  //       if (
+  //         location.hash === "#conceptual-overview" ||
+  //         location.hash === "#database-properties" ||
+  //         location.hash === "#enumerated-values" ||
+  //         location.hash === "#generate-access-key" ||
+  //         location.hash === "#error-codes" ||
+  //         location.hash === "#pagination" ||
+  //         location.hash === "#sorting" ||
+  //         location.hash === "#upcoming-deprecations" ||
+  //         location.hash === "#get-agents" ||
+  //         location.hash === "#get-agent" ||
+  //         location.hash === "#create-agent" ||
+  //         location.hash === "#update-agent" ||
+  //         location.hash === "#delete-agent" ||
+  //         location.hash === "#get-buildings" ||
+  //         location.hash === "#get-building" ||
+  //         location.hash === "#create-building" ||
+  //         location.hash === "#update-building" ||
+  //         location.hash === "#delete-building" ||
+  //         location.hash === "#get-customers" ||
+  //         location.hash === "#get-customer" ||
+  //         location.hash === "#create-customer" ||
+  //         location.hash === "#update-customer" ||
+  //         location.hash === "#delete-customer" ||
+  //         location.hash === "#get-leasables" ||
+  //         location.hash === "#get-leasable" ||
+  //         location.hash === "#create-leasable" ||
+  //         location.hash === "#update-leasable" ||
+  //         location.hash === "#delete-leasable" ||
+  //         location.hash === "#get-owners" ||
+  //         location.hash === "#get-owner" ||
+  //         location.hash === "#create-owner" ||
+  //         location.hash === "#update-owner" ||
+  //         location.hash === "#delete-owner" ||
+  //         location.hash === "#get-realestates" ||
+  //         location.hash === "#get-realestate" ||
+  //         location.hash === "#create-realestate" ||
+  //         location.hash === "#update-realestate" ||
+  //         location.hash === "#delete-realestate" ||
+  //         location.hash === "#get-regions" ||
+  //         location.hash === "#create-region" ||
+  //         location.hash === "#delete-region"
+  //       ) {
+  //         setOpen("navigation-sub-list-open");
+  //         console.log("haj");
+  //       } else {
+  //         setOpen("navigation-sub-list-closed");
+  //       }
+  //     } else if (
+  //       open === "navigation-sub-list-closed" &&
+  //       "#" + formatUrl(label) === location.hash
+  //     ) {
+  //       if (
+  //         location.hash === "#conceptual-overview" ||
+  //         location.hash === "#database-properties" ||
+  //         location.hash === "#enumerated-values" ||
+  //         location.hash === "#generate-access-key" ||
+  //         location.hash === "#error-codes" ||
+  //         location.hash === "#pagination" ||
+  //         location.hash === "#sorting" ||
+  //         location.hash === "#upcoming-deprecations" ||
+  //         location.hash === "#get-agents" ||
+  //         location.hash === "#get-agent" ||
+  //         location.hash === "#create-agent" ||
+  //         location.hash === "#update-agent" ||
+  //         location.hash === "#delete-agent" ||
+  //         location.hash === "#get-buildings" ||
+  //         location.hash === "#get-building" ||
+  //         location.hash === "#create-building" ||
+  //         location.hash === "#update-building" ||
+  //         location.hash === "#delete-building" ||
+  //         location.hash === "#get-customers" ||
+  //         location.hash === "#get-customer" ||
+  //         location.hash === "#create-customer" ||
+  //         location.hash === "#update-customer" ||
+  //         location.hash === "#delete-customer" ||
+  //         location.hash === "#get-leasables" ||
+  //         location.hash === "#get-leasable" ||
+  //         location.hash === "#create-leasable" ||
+  //         location.hash === "#update-leasable" ||
+  //         location.hash === "#delete-leasable" ||
+  //         location.hash === "#get-owners" ||
+  //         location.hash === "#get-owner" ||
+  //         location.hash === "#create-owner" ||
+  //         location.hash === "#update-owner" ||
+  //         location.hash === "#delete-owner" ||
+  //         location.hash === "#get-realestates" ||
+  //         location.hash === "#get-realestate" ||
+  //         location.hash === "#create-realestate" ||
+  //         location.hash === "#update-realestate" ||
+  //         location.hash === "#delete-realestate" ||
+  //         location.hash === "#get-regions" ||
+  //         location.hash === "#create-region" ||
+  //         location.hash === "#delete-region"
+  //       ) {
+  //         setOpen("navigation-sub-list-closed");
+  //       }
+  //       setOpen("navigation-sub-list-open");
+  //     } else if (
+  //       open === "navigation-sub-list-closed" &&
+  //       "#" + formatUrl(label) !== location.hash
+  //     ) {
+  //       setOpen("navigation-sub-list-closed");
+  //     }
+  //   }
+  // });
 
   const renderHeaderItem = () => {
     return (
