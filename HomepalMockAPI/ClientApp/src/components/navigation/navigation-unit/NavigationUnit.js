226c3f1596e9b6dae42f16956148e63f7f2cfe84
davidjungermann <david.jungermann@gmail.com>
2021-04-29 11:12:47 +0200

    Began changes

--- HomepalMockAPI/ClientApp/src/components/navigation/navigation-unit/NavigationUnit.js
+++ HomepalMockAPI/ClientApp/src/components/navigation/navigation-unit/NavigationUnit.js
@@ -2,12 +2,6 @@ import React, { useState } from "react";
 import { NavHashLink as Link } from "react-router-hash-link";
 import "./NavigationUnit.scss";
 
-export const NavigationUnit = ({
-  children
-}) => {
-  return (
-    <div className="navigation-unit-container">
-      
-    </div>
-  );
+export const NavigationUnit = ({ children }) => {
+  return <div className="navigation-unit-container">{children}</div>;
 };
